db00510a22eb061cd37a34c85e075f934a42992b
diegolagalera <diegolagalera_@hotmail.com>
2023-02-09 23:05:20 +0100

    commit

--- components/home/content3.vue
+++ components/home/content3.vue
@@ -7,7 +7,7 @@
     <div
       class="grid xl:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 grid-cols-1 text-black px-4 mt-8 md:text-gray-300 md:p-8 md:pt-8 lg:p-10 lg:pt-10 xl:p-20 xl:pt-4 gap-x-6 gap-y-4 customdivcontent3"
     >
-      <div>
+      <!-- <div>
         <Card></Card>
       </div>
       <div>
@@ -18,16 +18,16 @@
       </div>
       <div>
         <Card></Card>
-      </div>
+      </div> -->
     </div>
-    <video
+    <!-- <video
       class="hidden md:flex absolute top-0 xl:-mt-20 lg:mt-0 w-full -z-50"
       autoplay
       loop
       muted
     >
       <source src="/img/walk.mp4" type="video/mp4" />
-    </video>
+    </video> -->
 
     <!-- <video width="320" height="240" autoplay>
       <source src="/img/estronauta.mp4" type="video/mp4" />
